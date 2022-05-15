e342e5f702d77835c284919176a9e432ecb44e86
Kyle Back <back.kyle7@gmail.com>
2022-05-15 19:17:11 -0400

    Added navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'sporcle-playlist-maker';
+  title = 'sporcle-playlist-maker'
+  home_tab_selected: boolean = true
+
+  constructor(private router: Router) { }
+
+  public redirect(page: string) {
+    this.router.navigate(['./' + page])
+  }
+
+  public clickHome() {
+    if (!this.home_tab_selected) {
+      this.home_tab_selected = true
+      this.redirect('home')
+    }
+  }
+
+  public clickAbout() {
+    if (this.home_tab_selected) {
+      this.home_tab_selected = false
+      this.redirect('about')
+    }
+  }
 }
